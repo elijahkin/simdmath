e719e3c2e6886cc5649e0f47223fb323516a0990
Elijah Kin <elijah.m.kin@gmail.com>
2023-06-13 15:48:18 -0700

    Rectangularized

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,11 +1,19 @@
 /*
     @author Elijah Kin
-    @version 1.2
+    @version 1.3
     @date 06/13/23
 */
 
-#define BATCH_SIZE 8
-#define NUM_THREADS 8
+// Defining the constants we will need
+const int WIDTH = 2560;
+const int HEIGHT = 1600;
+const int NUM_THREADS = 8;
+const int BATCH_SIZE = 8;
+
+// Calculating derived constants
+const int SIZE = WIDTH * HEIGHT;
+const int THREAD_SIZE = SIZE / NUM_THREADS;
+const double ASPECT_RATIO = (double) HEIGHT / WIDTH;
 
 // #include <GLFW/glfw3.h>
 #include <immintrin.h>
@@ -14,8 +22,8 @@
 #include <thread>
 #include <vector>
 
-void iterate_simd(double * real, double * imag, double * escape_iter, int max_iter, int size) {
-    for (int i = 0; i < size; i += BATCH_SIZE) {
+void iterate_simd(double * real, double * imag, double * escape_iter, int max_iter) {
+    for (int i = 0; i < THREAD_SIZE; i += BATCH_SIZE) {
         // Loading real and imaginary values from memory
         __m512d C_RE = _mm512_load_pd(&real[i]);
         __m512d C_IM = _mm512_load_pd(&imag[i]);
@@ -55,13 +63,13 @@ void iterate_simd(double * real, double * imag, double * escape_iter, int max_it
     }
 }
 
-void mandelbrot_worker(double * real, double * imag, uint8_t * rgb, int max_iter, int size) {
+void mandelbrot_worker(double * real, double * imag, uint8_t * rgb, int max_iter) {
     // Allocating the memory this worker will need
-    double * escape_iter = (double *) malloc(size * sizeof(double));
+    double * escape_iter = (double *) malloc(THREAD_SIZE * sizeof(double));
     // Invoking the function to do iterations with SIMD intrisincs
-    iterate_simd(real, imag, escape_iter, max_iter, size);
+    iterate_simd(real, imag, escape_iter, max_iter);
     // Calculating RGB values from escape iterations
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < THREAD_SIZE; i++) {
         rgb[3 * i + 0] = 0;
         rgb[3 * i + 1] = (uint8_t) (255 * (escape_iter[i] / max_iter)) + 1;
         rgb[3 * i + 2] = 0;
@@ -69,7 +77,7 @@ void mandelbrot_worker(double * real, double * imag, uint8_t * rgb, int max_iter
     free(escape_iter);
 }
 
-void save_png(uint8_t * rgb, int width, int height, char * filename) {
+void save_png(uint8_t * rgb, char * filename) {
     FILE* fp = fopen(filename, "wb");
     png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
     png_infop info = png_create_info_struct(png);
@@ -77,8 +85,8 @@ void save_png(uint8_t * rgb, int width, int height, char * filename) {
     png_set_IHDR(
         png,
         info,
-        width,
-        height,
+        WIDTH,
+        HEIGHT,
         8,
         PNG_COLOR_TYPE_RGB,
         PNG_INTERLACE_NONE,
@@ -86,9 +94,9 @@ void save_png(uint8_t * rgb, int width, int height, char * filename) {
         PNG_FILTER_TYPE_DEFAULT
     );
     png_write_info(png, info);
-    png_bytep row_pointers [height];
-    for (int i = 0; i < height; i++) {
-        row_pointers[i] = &rgb[i * width * 3];
+    png_bytep row_pointers [HEIGHT];
+    for (int i = 0; i < HEIGHT; i++) {
+        row_pointers[i] = &rgb[i * WIDTH * 3];
     }
     png_write_image(png, row_pointers);
     png_write_end(png, NULL);
@@ -96,26 +104,29 @@ void save_png(uint8_t * rgb, int width, int height, char * filename) {
     fclose(fp);
 }
 
-void mandelbrot(double center_real, double center_imag, double apothem, int max_iter, int n) {
-    int size = n * n;
+void mandelbrot(double center_real, double center_imag, double apothem, int max_iter) {
     // Allocating the memory we will need
-    double * real = (double *) malloc(size * sizeof(double));
-    double * imag = (double *) malloc(size * sizeof(double));
-    uint8_t * rgb = (uint8_t *) malloc(3 * size);
-    // Initiliazing the real and imaginary values
+    double * real = (double *) malloc(SIZE * sizeof(double));
+    double * imag = (double *) malloc(SIZE * sizeof(double));
+    uint8_t * rgb = (uint8_t *) malloc(3 * SIZE);
+    // Computing values we need to initialize real and imag
     double min_real = center_real - apothem;
-    double max_imag = center_imag + apothem;
-    double step_size = 2 * apothem / (n - 1);
-    for (int i = 0; i < size; i++) {
-        real[i] = min_real + (i % n) * step_size;
-        imag[i] = max_imag - (i / n) * step_size;
+    double max_imag = center_imag + apothem * ASPECT_RATIO;
+    double real_step = 2 * apothem / (WIDTH - 1);
+    double imag_step = 2 * apothem / (HEIGHT - 1) * ASPECT_RATIO;
+    // Initializing the real and imag values
+    int row, col;
+    for (int i = 0; i < SIZE; i++) {
+        col = i % WIDTH;
+        row = i / WIDTH;
+        real[i] = min_real + col * real_step;
+        imag[i] = max_imag - row * imag_step;
     }
     // Setting up the threads
-    int thread_size = size / NUM_THREADS;
     std::vector<std::thread> threads;
     for (int i = 0; i < NUM_THREADS; i++) {
-        threads.push_back(std::thread(mandelbrot_worker, &real[i*thread_size],
-                    &imag[i*thread_size], &rgb[i*3*thread_size], max_iter, thread_size));
+        threads.push_back(std::thread(mandelbrot_worker, &real[i * THREAD_SIZE],
+                            &imag[i * THREAD_SIZE], &rgb[i * 3 * THREAD_SIZE], max_iter));
     }
     // Waiting for the threads to finish
     for (std::thread& thread : threads) {
@@ -127,18 +138,18 @@ void mandelbrot(double center_real, double center_imag, double apothem, int max_
     free(imag);
     // Generating the name for the image file
     char filename[100];
-    sprintf(filename, "renders/mandelbrot (%.02f, %.02f, %.02f, %i, %i, %i).png",
-            center_real, center_imag, apothem, max_iter, n, n);
+    sprintf(filename, "renders/mandelbrot_%i_%i_%i (%.02f, %.02f, %.02f).png",
+            max_iter, WIDTH, HEIGHT, center_real, center_imag, apothem);
     // Saving the RGB data to the image
-    save_png(rgb, n, n, filename);
+    save_png(rgb, filename);
     free(rgb);
 }
 
 int main() {
     auto start = std::chrono::steady_clock::now();
-    // mandelbrot(-0.6, 0, 1.5, 200, 8192);
-    // mandelbrot(-0.77568377, 0.13646737, 0.0000001, 1000, 4096);
-    mandelbrot(0.001643721971153, -0.822467633298876, 0.00000000002, 1600, 4096);
+    mandelbrot(-0.6, 0, 2, 200);
+    mandelbrot(-0.77568377, 0.13646737, 0.0000001, 1000);
+    mandelbrot(0.001643721971153, -0.822467633298876, 0.00000000002, 1600);
     auto end = std::chrono::steady_clock::now();
     std::cout << (end - start).count() / 1000000 << " ms" << std::endl;
     return 0;
