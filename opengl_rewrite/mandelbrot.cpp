4a652257f81e3b6615e1f20b63ed256f02785a22
Elijah Kin <elijah.m.kin@gmail.com>
2023-07-02 10:39:21 -0700

    Add color cycling

--- opengl_rewrite/mandelbrot.cpp
+++ opengl_rewrite/mandelbrot.cpp
@@ -2,8 +2,8 @@
 
 #include <GLUT/glut.h>
 #include <stdlib.h>
+#include <cmath>
 
-// Function to handle window resizing
 void reshape(int width, int height) {
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
@@ -11,44 +11,46 @@ void reshape(int width, int height) {
     gluOrtho2D(0, width, 0, height);
 }
 
-// Function to render the scene
 void render() {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Draw a triangle
+    // Calculate color values based on time
+    float time = glutGet(GLUT_ELAPSED_TIME) / 1000.0;  // Get elapsed time in seconds
+    float red = sin(time * 2.0);  // Varying value between -1.0 and 1.0
+    float green = sin(time * 0.7); // Varying value between -1.0 and 1.0
+    float blue = sin(time * 1.3);  // Varying value between -1.0 and 1.0
+
+    // Draw a triangle with dynamically changing color
     glBegin(GL_TRIANGLES);
-    glColor3f(1.0, 0.0, 0.0);  // Red color
+    glColor3f((red + 1.0) / 2.0, (green + 1.0) / 2.0, (blue + 1.0) / 2.0);
     glVertex2f(100, 100);
-    glColor3f(0.0, 1.0, 0.0);  // Green color
     glVertex2f(2460, 100);
-    glColor3f(0.0, 0.0, 1.0);  // Blue color
     glVertex2f(100, 1500);
     glEnd();
 
     glFlush();
 }
 
-// Function to handle keyboard events
 void keyboard(unsigned char key, int x, int y) {
-    if (key == 27) {  // ASCII code for Escape key
+    if (key == 27) {  // esc
         exit(0);
     }
 }
 
 int main(int argc, char** argv) {
-    // Initialize GLUT
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
-    glutGameModeString("2560x1600:32");
-    glutEnterGameMode();
+	glutInitWindowSize(400,300);
+	glutCreateWindow("Mandelbrot");
+    // glutGameModeString("2560x1600:32");
+    // glutEnterGameMode();
 
-    // Set callback functions
-    glutReshapeFunc(reshape);
     glutDisplayFunc(render);
+    glutIdleFunc(render);
     glutKeyboardFunc(keyboard);
+    glutReshapeFunc(reshape);
 
-    // Enter the main loop
     glutMainLoop();
 
     return 0;
